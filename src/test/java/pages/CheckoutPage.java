2690ed2fb6544f85904e06f6dd0b5d6490a6fefe
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-07-08 09:52:30 +0530

    First Commit

--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -12,10 +12,20 @@ public class CheckoutPage
     }
 
     private final By checkoutTitleField = By.xpath("//span[text()='Checkout: Your Information']");
+    private final By firstNameField = By.id("first-name");
+    private final By lastNameField = By.id("last-name");
+    private final By postalCodeField = By.id("postal-code");
+    private final By continueToCheckout2Field = By.id("continue");
+    private final By backToCartField = By.id("cancel");
+
+
+
 
     public boolean isCheckoutTitleDisplayed()
     {
         return driver.findElement(checkoutTitleField).isDisplayed();
     }
+
+
 
 }
--- src/test/java/stepdefinitions/CheckoutStepDefinition.java
+++ src/test/java/stepdefinitions/CheckoutStepDefinition.java
@@ -1,6 +1,29 @@
 package stepdefinitions;
 
+import context.ScenarioContext;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+
 public class CheckoutStepDefinition
 {
+    ScenarioContext scenarioContext;
+    @When("user navigates to checkout page")
+    public void user_navigates_to_checkout_page() {
+        // Write code here that turns the phrase above into concrete actions
+        Assertions.assertTrue(scenarioContext.getCheckoutPage().isCheckoutTitleDisplayed(), "Checkout Page not loaded");
+    }
+
+    @When("user enters first name {string} and last name {string} and postal code {string}")
+    public void user_enters_first_name_and_last_name_and_postal_code(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user should see all the details in checkout overview")
+    public void user_should_see_all_the_details_in_checkout_overview() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
